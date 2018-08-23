096a72f0bc10be1db5e4a1e360cc7a8ec4f21ae7
eladgubkin <Eladox3@gmail.com>
2018-08-23 22:33:20 +0300

    Flask

--- flask_/myFlask/forms.py
+++ flask_/myFlask/forms.py
@@ -21,14 +21,12 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError(
-                'That username is taken. Please choose a different one.')
+            raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError(
-                'That email is taken. Please choose a different one.')
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
@@ -56,15 +54,13 @@ class UpdateAccountForm(FlaskForm):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError(
-                    'That username is taken. Please choose a different one.')
+                raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError(
-                    'That email is taken. Please choose a different one.')
+                raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class PostForm(FlaskForm):
--- flask_/myFlask/models.py
+++ flask_/myFlask/models.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin
 
 
 @login_manager.user_loader
-def loal_user(user_id):
+def load_user(user_id):
     return User.query.get(int(user_id))
 
 
@@ -12,8 +12,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False,
-                           default='default.png')
+    image_file = db.Column(db.String(20), nullable=False, default='default.png')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
@@ -24,8 +23,7 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False,
-                            default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
